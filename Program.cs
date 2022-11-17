bdcc74ab20125b8aeff83e1b33d0fc04306e9079
Felipe Bausen Boldt <109118733+felipethefever@users.noreply.github.com>
2022-11-16 23:38:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OlaMundo
+
+{
+
+    class Ola
+
+    {
+
+        static void Main()
+
+        {
+
+            Console.WriteLine("Ola Mundo");
+
+            Console.WriteLine("Pressione qualquer tecla para sair");
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
